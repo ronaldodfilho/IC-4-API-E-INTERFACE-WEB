ddaf582c432e1d7747a16067fcdecc2d34f09eb6
ronaldodfilho <ronaldodfilho@hotmail.com>
2026-07-20 17:49:35 -0300

    feat: implementa rotas principais da API

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,116 @@
-from fastapi import FastAPI;
-from banco import conectar;
+from fastapi import FastAPI, HTTPException
+from banco import conectar
 
-app = FastAPI();
-
-@app.get("/")
-def inicio():
-  return {"message": "Hello World!"};
-
-@app.get("/api/teste-banco")
-def teste_banco():
-  conexao = conectar();
-  cursor = conexao.cursor();
+app = FastAPI()
   
-  cursor.execute("SELECT COUNT(*) FROM operadoras;")
-  resultado = cursor.fetchone();
+@app.get("/api/operadoras")
+def listar_operadoras(page: int = 1, limit: int = 10):
+    offset = (page - 1) * limit
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+    
+    cursor.execute("SELECT * FROM operadoras LIMIT %s OFFSET %s", (limit, offset))
+    operadoras = cursor.fetchall()
+    
+    cursor.execute("SELECT COUNT(*) AS total FROM operadoras;")
+    resultado = cursor.fetchone()
   
-  cursor.close();
-  conexao.close();
+    cursor.close()
+    conexao.close()
   
-  return {
-    "Menssagem": "Conexão realizada com sucesso!",
-    "Total de operadoras": resultado[0]
-  }
+    return {
+      "data": operadoras,
+      "total" : resultado["total"],
+      "page": page,
+      "limit": limit,
+    }
+    
+@app.get("/api/operadoras/{cnpj}")
+def buscar_operadora(cnpj : str):
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+    
+    cursor.execute("SELECT * FROM operadoras WHERE cnpj = %s", (cnpj,))
+    operadora = cursor.fetchone()
+    
+    cursor.close()
+    conexao.close()
+    
+    if operadora is None:
+      raise HTTPException(status_code=404, detail="Operadora não encontrada")
+    
+    return {
+      "data": operadora
+    }
+    
+@app.get("/api/operadoras/{cnpj}/despesas")
+def buscar_despesas_operadora(cnpj: str):
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+    
+    cursor.execute("""
+                  SELECT o.cnpj, o.razao_social, dc.ano, dc.trimestre, dc.valor_despesas FROM despesas_consolidadas AS dc
+                  JOIN operadoras o 
+                  ON o.id = dc.operadora_id
+                  WHERE o.cnpj = %s
+                  ORDER BY dc.ano DESC, dc.trimestre DESC
+                  """, (cnpj,))
+    
+    despesas = cursor.fetchall()
+    
+    cursor.close()
+    conexao.close()
+    
+    if not despesas:
+        raise HTTPException(status_code=404, detail="Despesas não encontradas para a operadora")
+      
+    return {
+        "data": despesas
+      }
+    
+@app.get("/api/estatisticas")
+def estatisticas():
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+    
+    cursor.execute("""
+                   SELECT SUM(valor_despesas) AS total_despesas,
+                   AVG(valor_despesas) AS media_despesas
+                   FROM despesas_consolidadas;     
+                   """)
+    
+    estatisticas = cursor.fetchone()
+    
+    cursor.execute("""
+                   SELECT o.cnpj, o.razao_social,SUM(dc.valor_despesas) AS total_despesas
+                    FROM despesas_consolidadas AS dc
+                    JOIN operadoras AS o
+                      ON o.id = dc.operadora_id
+                    GROUP BY o.cnpj, o.id, o.razao_social
+                    ORDER BY total_despesas DESC
+                    LIMIT 5;
+                   """)
+    
+    top_operadoras = cursor.fetchall()
+    
+    cursor.execute("""SELECT 
+                      o.uf,
+                      ROUND(SUM(da.total_despesas), 2) AS total_despesas_uf,
+                      ROUND(AVG(da.total_despesas), 2) AS media_despesas_por_operadora
+                      FROM despesas_agregadas AS da
+                      JOIN operadoras AS o
+                        ON o.id = da.operadora_id
+                      GROUP BY o.uf
+                      ORDER BY total_despesas_uf DESC;""")
+    
+    distribuicao_uf = cursor.fetchall()
+    
+    cursor.close()
+    conexao.close()
+    
+    return {
+        "total despesas": estatisticas["total_despesas"],
+        "media despesas": estatisticas["media_despesas"],
+        "top_operadoras": top_operadoras,
+        "distribuicao_uf": distribuicao_uf
+    }
